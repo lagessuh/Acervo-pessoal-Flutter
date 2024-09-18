f6ab599d02dead95dfea233972ee3536202336b7
Suellen Lages <suellenlages.9@gmail.com>
2024-09-18 00:17:37 -0400

    Alterações na MainPage

--- lib/pages/main/main_page.dart
+++ lib/pages/main/main_page.dart
@@ -66,7 +66,7 @@ class _MainPageState extends State<MainPage> {
           ),
           NavigationDestination(
             icon: Icon(Icons.person),
-            label: 'Profile',
+            label: 'Perfil',
           ),
         ],
       ),
